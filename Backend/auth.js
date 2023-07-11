0a0e2a0d796868bd2338983d701e8be22bff416b
Rosmontis <58333458+Minecrafter-Pythoner@users.noreply.github.com>
2023-07-11 18:18:30 +0800

    MAJOR UPDATE 1.0 - Removed useless code - Wrote QueryTasksByDate - Used Date&Time Server Native Input - Most APIs working correctly(if not all)

--- Backend/auth.js
+++ Backend/auth.js
@@ -9,21 +9,8 @@ function checkUser(req, res, next) {
   }
 }
 
-async function authorizeUser(req, res, next) {
-  if(req.session && req.session.user) {
-    const userId = req.session.user;
-    next(userId);
-  }
-  else {
-    res.status(401)
-    res.send("401 Unauthorized")
-  }
-
-}
-
 // Export the middleware function(s)
 const auth = {
   checkUser,
-  authorizeUser
 };
 module.exports = auth;
--- Backend/router.js
+++ Backend/router.js
@@ -2,19 +2,14 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-
 const ctrl = require('./controller')
 const auth = require('./auth')
 
 // Define routes and map them to the corresponding functions
 router.get('/', (req, res) => {
-  if (req.session && req.session.user) {res.sendFile(path.join(__dirname, 'dashboard.html'));}
-  else {res.sendFile(path.join(__dirname, 'login.html'));}
+  if (req.session && req.session.user) { res.redirect('/dashboard'); }
+  else { res.redirect('/login'); }
 })
-
-
-// Route for user login
-router.post('/login', ctrl.login);
 
 router.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'login.html'));
@@ -24,6 +19,9 @@ router.get('/signup', (req, res) => {
   res.sendFile(path.join(__dirname, 'signup.html'));
 });
 
+// Route for user login
+router.post('/login', ctrl.login);
+
 router.post('/signup', ctrl.register);
 
 router.get('/dashboard', (req, res) => {
@@ -31,25 +29,17 @@ router.get('/dashboard', (req, res) => {
 });
 
 // Route for changing password
-router.post('/change-password', ctrl.changePassword);
+router.post('/change-password',auth.checkUser, ctrl.changePassword);
 
 // Route for adding a task
 //router.post('/add-task', auth.checkUser, ctrl.addTask);
 router.post('/add-task', auth.checkUser, ctrl.addTask);
 
 // Route for showing tasks by date
-router.get('/tasks/:date', auth.checkUser, ctrl.showTaskByDate);
+router.get('/tasks', auth.checkUser, ctrl.showTaskByDate);
 
 // Route for deleting a task
-//router.delete('/tasks/:taskId', auth.checkUser, ctrl.deleteTask);
 router.delete('/delete-task', auth.checkUser, ctrl.deleteTask);
-
-
-// Route for scheduling a reminder
-router.post('/tasks/:taskId/reminders', ctrl.reminders);
-
-// Route for sending a reminder
-router.post('/users/:userId/reminders', ctrl.sendReminder);
 
 router.get('/query-tasks', ctrl.queryTasks);
 
